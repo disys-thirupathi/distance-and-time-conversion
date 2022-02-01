6ac145dd85429d943a70f45933bb8ac96b0fe835
thirupathi <thirupathi.narayanan@disys.com>
2022-02-01 18:20:58 +0530

    Add project files.

---
+++ jan17/distance.cs
@@ -0,0 +1,76 @@
+﻿using System;
+abstract class Distance_converter //Abstract class created
+{
+    public abstract double m_km(double input);
+    public static double km_m(double input);
+    public static double mi_km(double input);
+    public static double km_mi(double input);
+}
+class conversion : Distance_converter
+{
+    public override double m_km(double input)
+    {
+        double km = 0;
+        km = input / 1000;
+        Console.WriteLine(" Result in Kilometer " + km);
+    }
+
+    public override double km_m(double input)
+    {
+        double m = 0;
+        m = input * 1000;
+        Console.WriteLine(" Result in Meter " + m);
+    }
+
+    public override double mi_km(double input)
+    {
+        double m = 0;
+        km = input * 1.609;
+        Console.WriteLine(" Result in Kilometer " + km);
+    }
+
+    public override double km_mi(double input)
+    {
+        double mi = 0;
+        mi = input / 1.609;
+        Console.WriteLine(" Result in Mile " + mi);
+    }
+}
+class user_main
+{
+
+    static void Main()
+    {
+        conversion c = new conversion();
+        Console.WriteLine("Distance convertion Calculator");
+        Console.WriteLine("1. Kilometer to Meter\n 2. Meter to Kilometer\n3. Mile to Kilometer\n4. Kilometer to Mile");
+        int choice = Convert.ToInt32(Console.ReadLine());
+        double input, m = 0, km = 0, mi = 0;
+        switch (choice)
+        {
+            case 1:
+                Console.WriteLine("Enter Kilometer \n");
+                input = double.Parse(Console.ReadLine());
+                m = c.km_m(input);
+                break;
+
+            case 2:
+                Console.WriteLine("Enter Meter \n");
+                input = double.Parse(Console.ReadLine());
+                km = c.m_km(input);
+                break;
+
+            case 3:
+                Console.WriteLine("Enter Mile \n");
+                input = double.Parse(Console.ReadLine());
+                km = c.mi_km(input);
+                break;
+
+            case 4:
+                Console.WriteLine("Enter Kilometer \n");
+                input = double.Parse(Console.ReadLine());
+                mi = c.km_mi(input);
+                break;
+        }
+    }
+}
---
+++ time conversion.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace time_conv
+{
+    public abstract class time
+    {
+        public abstract double hr_m();
+        public abstract int m_hr();
+        public abstract int m_se();
+        public abstract int se_m();
+    }
+    public class time : time_convert
+    {
+        public abstract override double hr_m(d)
+        {
+            int minute = 0;
+            minute = hour * 60;
+            Console.WriteLine("Hour in Minutes : " + minute);
+        }
+        public abstract override int m_hr()
+        {
+            double hour = 0;
+            hour = min / 60;
+            Console.WriteLine("Minutes in hour : " + hour);
+        }
+        public abstract override int m_se()
+        {
+            double second = 0;
+            second = min * 60;
+            Console.WriteLine("Minutes in second : " + second);
+        }
+        public abstract override int se_m()
+        {
+            double min = 0;
+            min = sec / 60;
+            Console.WriteLine("second in Minutes : " + min);
+        }
+    }
+    public class HelloWorld
+    {
+        public static void Main(string[] args)
+        {
+            time tc = new time_convert();
+            tc.hr_m(2.25);
+            tc.m_hr(75);
+            tc.m_se(88);
+            tc.se_m(99);
+        }
+    }
+}
